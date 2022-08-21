59ec6e915f533fb997d47bf614c021040e22800d
linq <royal@perennial-tech.com>
2022-08-21 18:32:21 +0800

    updates

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 function handleKeyDown() {
-<<<<<<< HEAD
-
     if (event.key === 'Enter') {
       location.href = "https://impervious.domains/name/" + event.target.value + ".lilnode/register/";
     }
   };
 
-=======
-  if (event.key === 'Enter') {
-    location.href = "https://impervious.domains/name/" + event.target.value + ".wwww/register/";
-  }
-};
->>>>>>> parent of 221125a7 (update)
